6cd19df315dbf5478ba47acc7a1cde6b083c3dff
BVAk <bekkviktoria55@gmail.com>
2019-07-08 22:13:55 +0300

    ToDo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+   <HelloWorld msg="Welcome, my lovely Andrew"/>
+  <todo></todo>
    <button-counter></button-counter>
   </div>
 </template>
