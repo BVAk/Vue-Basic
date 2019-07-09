7149c7d81e7acc6d89fa809adaf6821a3067fec7
BVAk <bekkviktoria55@gmail.com>
2019-07-09 20:32:39 +0300

    Delete item and reverse list

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -3,7 +3,9 @@
     <h1>Todo App</h1>
     <input @keyup.enter="add" type="text" v-model="title" placeholder="What you want to do?">
     <button @click="add">Add to list</button>
-<ul><li v-for="todo in todos">{{todo}}</li></ul>
+<ul><li v-for="(todo,index) in todos">{{todo}}
+  <span @click="remove(index)" class="remove">x</span>
+</li></ul>
 </div>  
 </template>
 
@@ -22,9 +24,18 @@ export default {
     }
   },
   methods:{
+    remove(id){
+      //console.log(id);
+    //  this.todos.splice(id,1);
+      this.$delete(this.todos, id);
+
+    },
     add(){
-      this.todos.push(this.title),
-this.title=""
+      if(this.title!=''){
+       //this.todos.push(this.title),
+        this.todos.unshift(this.title),
+this.title=""}
+           
     }
   }
 };
@@ -32,5 +43,12 @@ this.title=""
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.remove{
+  padding: .3rem .6rem;
+  background-color: red;
+  color: white;
+  border-radius: 30px;
+}
+.remove:hover{cursor: pointer;}
+ul li{padding: .4rem;}
 </style>
